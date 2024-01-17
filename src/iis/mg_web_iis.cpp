0c3aa58cb32f48a58c1c9951d468980b399fd68d
chrisemunt <cmunt@mgateway.com>
2024-01-17 15:18:30 +0000

    Add files via upload

--- src/iis/mg_web_iis.cpp
+++ src/iis/mg_web_iis.cpp
@@ -4,7 +4,7 @@
    | Description: IIS HTTP Gateway for InterSystems Cache/IRIS and YottaDB    |
    | Author:      Chris Munt cmunt@mgateway.com                               |
    |                         chris.e.munt@gmail.com                           |
-   | Copyright (c) 2019-2023 MGateway Ltd                                     |
+   | Copyright (c) 2019-2024 MGateway Ltd                                     |
    | Surrey UK.                                                               |
    | All rights reserved.                                                     |
    |                                                                          |
